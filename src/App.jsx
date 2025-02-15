677f25475523145d18f74b147fec3745add56cda
alex100588 <alexandru_ionasc@yahoo.com>
2025-02-15 19:05:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return ( 
+    <Navbar />
+   );
+}
+ 
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+import logo from '../../assets/logo.svg';
+
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <img src={logo} alt="" />
+        </div>
+     );
+}
+ 
+export default Navbar;

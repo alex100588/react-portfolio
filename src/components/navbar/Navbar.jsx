9986a754f2328249a226d2d12eeab2cc520d4faf
alex100588 <alexandru_ionasc@yahoo.com>
2025-02-15 19:26:01 +0200

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,14 @@ const Navbar = () => {
     return ( 
         <div className="navbar">
             <img src={logo} alt="" />
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>About me</li>
+                <li>Education</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+            <div className="nav-conect">Connect with me</div>
         </div>
      );
 }

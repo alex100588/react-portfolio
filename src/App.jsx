f1f23406b5e57610796e6ad9177af8a9c026a707
alex100588 <alexandru_ionasc@yahoo.com>
2025-02-27 16:54:28 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
@@ -14,6 +15,7 @@ const App = () => {
       <Education />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 };

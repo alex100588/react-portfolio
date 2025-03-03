5382ef45d7521b0da86d0f57c95441f9757d8aed
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-03 16:14:36 +0200

    design update

--- src/components/services/Education.jsx
+++ src/components/services/Education.jsx
@@ -1,6 +1,6 @@
 import "./Education.css";
 import licenta from "../../assets/licenta.jpeg";
-import game2 from "../../assets/teachBit.png";
+import webDevelopment from "../../assets/teachBit.png";
 
 const Services = () => {
   return (
@@ -8,17 +8,29 @@ const Services = () => {
       <h1 className="h1-design">My education</h1>
       <div id="education" className="my-education">
         <div className="education-title">
-          <h1>My Education</h1>
+          <h1>Bachelor's degree</h1>
+          <p>
+            On my journey I learned the basics of programming languages, <br />{" "}
+            databases an networking fundamentals
+          </p>
+          <br />
           <img src={licenta} alt="licence" />
         </div>
         <div className="certifications">
           <div className="certificate">
-            <h3>Certification 1</h3>
-            <img src={game2} alt="" />
+            <h3>Web Development</h3>
+            <p>
+              The course helped me to improve myself <br /> by learning techical
+              skills and soft skills, <br />
+              I learned how to put in practice and create  <br /> applications with <br /> Html, Css, Javascript, Bootstrap and React
+            </p>
+            <br />
+            <img src={webDevelopment} alt="" />
           </div>
           <div>
-            <h3>Certification 2</h3>
-            <img src={game2} alt="" />
+            <h3>React with Typescript</h3>
+            <p></p>
+            <img src={webDevelopment} alt="" />
           </div>
         </div>
       </div>
